56ce19a6ade7880543ce5aa2b6b9352c20bf0e0b
sumit sengar <87333260+sengarsumit@users.noreply.github.com>
2025-01-19 17:23:32 +0530

    api

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -26,7 +26,7 @@ userSchema.pre("save",async function(next){
     next();
 })
 userSchema.methods.isPasswordCorrect=async function(password){
-    await bcrypt.compare(password,this.password);
+    return await  bcrypt.compare(password,this.password);
 }
 userSchema.methods.generateAccessToken= function(){
        return jwt.sign({_id:this._id,email:this.email,username:this.username,fullname:this.fullname},process.env.ACCESS_TOKEN_SECRET,{expiresIn:process.env.ACCESS_TOKEN_EXPIRY})
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import {Router} from "express"
 import {upload} from "../middlewares/multer.middleware.js"
-import {registerUser,logoutUser} from "../controllers/user.controller.js"
+import {registerUser,logoutUser,loginUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDeatils,
+    getUserChannelProfile, updateUserAvatar, updateUserCoverImage, getWatchHistory,} from "../controllers/user.controller.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 
 const router=Router()
@@ -13,8 +14,17 @@ router.route("/register").post(upload.fields([
         maxCount:1
     }
 ]),registerUser)
+router.route("/login").post(loginUser)
+router.route("/refresh-token").post(refreshAccessToken)
+
 
 //secured routes
 router.route("/logout").post(verifyJWT,logoutUser)
-
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+router.route("/update-account").patch(verifyJWT,updateAccountDeatils)
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+router.route("/history").get(verifyJWT,getWatchHistory)
 export default router
